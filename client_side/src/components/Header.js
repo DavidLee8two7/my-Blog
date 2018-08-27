fd3a18af9703307b64335e52c4cc5d4cc4d09851
David Lee <DavidLee8two7@gmail.com>
2018-08-26 22:26:19 -0700

    resetting the boilerplate create-react-app

---
+++ client_side/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return (
+			<div>
+				<header className="header">
+					<h1 className="header--title">This is header</h1>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default Header;
