457515ebfadb934a56bb710d286cbc9b4872654c
David Lee <DavidLee8two7@gmail.com>
2018-08-28 21:07:16 -0700

    Header image

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 import './App.scss';
 
 import Sidebar from '../src/components/Sidebar';
+import Header from '../src/components/Header';
+import IntroPhoto from '../src/components/IntroPhoto';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Sidebar />
+				<Header />
+				<IntroPhoto />
 			</div>
 		);
 	}
