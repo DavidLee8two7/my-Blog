2d2518f9ca8736aceb5b2173aa31469a77b0aac8
David Lee <DavidLee8two7@gmail.com>
2018-09-09 21:36:28 -0700

    landing-page layout is completed

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+
 import './App.scss';
 
-import Sidebar from '../src/components/Sidebar';
 import Header from '../src/components/Header';
+import Sidebar from '../src/components/Sidebar';
 import Profile from '../src/components/Profile';
-import Skills from '../src/components/Skills';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Sidebar />
 				<Header />
+				<Sidebar />
 				<Profile />
-				<Skills />
 			</div>
 		);
 	}
