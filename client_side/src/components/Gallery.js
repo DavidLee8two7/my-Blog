d24b68d5c27a147309fc8dbaf1948f7230667fe8
David Lee <DavidLee8two7@gmail.com>
2018-09-11 19:48:14 -0700

    Refacoring the landing page

---
+++ client_side/src/components/Gallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallery = () => (
+	<section className="gallery">
+		<figure className="gallery__item">
+			<img src="img/with-wife.jpeg" alt="myPic-1" className="gallery__img" />
+		</figure>
+	</section>
+);
+
+export default Gallery;
