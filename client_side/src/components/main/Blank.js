bc503a89ddfc8442a98f53fd15a9cd9b2b3fe135
David Lee <DavidLee8two7@gmail.com>
2018-11-24 01:25:15 -0800

    react/next & graphql/prisma setup

--- client_side/src/components/main/Blank.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Blank extends Component {
-  render() {
-    return <div className="blank">blank</div>;
-  }
-}
-
-export default Blank;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Index = props => (
+  <div>
+    <Link href="/home">
+      <a>home</a>
+    </Link>
+  </div>
+);
+
+export default Index;
