7203e0bf9c8aaa3e56b6cbe2e699e5bf0d7f6c3c
David Lee <DavidLee8two7@gmail.com>
2018-12-08 14:59:47 -0800

    blogs page

---
+++ frontend/components/ContactPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Head from "next/head";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>David Lee | Contact</title>
+        </Head>
+        <div>Contact1</div>
+        <div>Contact2</div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
