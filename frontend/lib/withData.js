be601a3dd1ca77af38ea2d9cce07c929e8229a88
David Lee <DavidLee8two7@gmail.com>
2019-02-18 19:31:44 -0800

    updated

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,8 +1,6 @@
 import withApollo from "next-with-apollo";
 import ApolloClient from "apollo-boost";
 import { endpoint, prodEndpoint } from "../config";
-import { LOCAL_STATE_REACT } from "../components/skillsPage/ReactStack";
-import { LOCAL_STATE_NODE } from "../components/skillsPage/NodeStack";
 
 function createClient({ headers }) {
   return new ApolloClient({
@@ -14,36 +12,6 @@ function createClient({ headers }) {
         },
         headers
       });
-    },
-    clientState: {
-      resolvers: {
-        Mutation: {
-          toggleReact(_, variables, { cache }) {
-            const { cardReact } = cache.readQuery({
-              query: LOCAL_STATE_REACT
-            });
-            const data = {
-              data: { cardReact: !cardReact }
-            };
-            cache.writeData(data);
-            return data;
-          },
-          toggleNode(_, variables, { cache }) {
-            const { cardNode } = cache.readQuery({
-              query: LOCAL_STATE_NODE
-            });
-            const data = {
-              data: { cardNode: !cardNode }
-            };
-            cache.writeData(data);
-            return data;
-          }
-        }
-      },
-      defaults: {
-        cardReact: false,
-        cardNode: false
-      }
     }
   });
 }
